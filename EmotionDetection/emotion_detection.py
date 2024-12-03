21b28d92beea2da2bf15053f2f875495d97c6b34
Kewei Zhang <jeffzhkw@gmail.com>
2024-12-03 16:06:58 -0500

    Complete Task 6

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,27 @@
+import requests
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = {"raw_document": {"text": text_to_analyze}}
+    try:
+        response = requests.post(url, headers=headers, json=input_json)
+        response.raise_for_status()
+
+        response_data = response.json()
+        emotion_predictions = response_data.get('emotionPredictions')
+        res_emotion = emotion_predictions[0].get('emotion')
+
+        dominant_emotion_name = max(res_emotion, key=res_emotion.get)
+        
+        return_res = {
+            'anger': res_emotion.get('anger'),
+            'disgust': res_emotion.get('disgust'),
+            'fear': res_emotion.get('fear'),
+            'joy': res_emotion.get('joy'),
+            'sadness': res_emotion.get('sadness'),
+            'dominant_emotion': dominant_emotion_name
+        }
+        return return_res
+    except requests.exceptions.RequestException as e:
+        print(e)
---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, render_template
+from EmotionDetection import emotion_detector
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def website():
+    return render_template('index.html')
+
+@app.route('/emotionDetector')
+def emotionDetector():
+    #text = "I love my life"
+    text = request.args.get('textToAnalyze')
+    result = emotion_detector(text)
+    return (f"For the given statement, the system response is 'anger': {result['anger']}, 'disgust': {result['disgust']}, "
+            f"'fear': {result['fear']}, 'joy': {result['joy']} and 'sadness': {result['sadness']}. "
+            f"The dominant emotion is {result['dominant_emotion']}.")
+
+if __name__ == '__main__':
+    app.run(debug=True)
