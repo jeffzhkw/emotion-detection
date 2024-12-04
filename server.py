39d6ef4972ae7275ffbd0cf336b49fef6d611b4a
Kewei Zhang <kz1091@nyu.edu>
2024-12-03 20:40:44 -0500

    Complete Task 8

--- server.py
+++ server.py
@@ -1,3 +1,8 @@
+"""
+server.py
+This is a Flask Application 
+"""
+
 from flask import Flask, request, render_template
 from EmotionDetection import emotion_detector
 
@@ -6,16 +11,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def website():
+    """
+    Serves the webpage
+    """
     return render_template('index.html')
 
 @app.route('/emotionDetector')
-def emotionDetector():
+def emotion_detector_function():
+    """
+    Handles API request
+    """
     #text = "I love my life"
     text = request.args.get('textToAnalyze')
     result = emotion_detector(text)
     if not result['dominant_emotion']:
         return "Invalid text! Please try again!"
-    return (f"For the given statement, the system response is 'anger': {result['anger']}, 'disgust': {result['disgust']}, "
+    return (f"For the given statement, the system response is 'anger': {result['anger']}, "
+            f"'disgust': {result['disgust']}, "
             f"'fear': {result['fear']}, 'joy': {result['joy']} and 'sadness': {result['sadness']}. "
             f"The dominant emotion is {result['dominant_emotion']}.")
 
